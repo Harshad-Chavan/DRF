a83398a24f70aeeb74e19689347579bcc982abec
harshad <harshadchavan141@gmail.com>
2023-10-27 20:36:16 +0530

    restart

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("api_home", views.api_home, name="api_home")
-]
-
--- python_client/basic.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-endpoint = "http://127.0.0.1:8000/api/api_home"
-
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "this is a query"})
-print(get_response.text)
-print(get_response.json())
-print(get_response.status_code)
